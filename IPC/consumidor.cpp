83b501a3bf31124bdacbe6127483883cc2399372
Naninh4 <liviaeusla@gmail.com>
2024-07-12 17:01:24 -0300

    if 12/07

---
+++ IPC/consumidor.cpp
@@ -0,0 +1,12 @@
+/* produtor/consumidor: Programa exemplo de memória compartilhada */
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+
+int main(){
+    
+}
