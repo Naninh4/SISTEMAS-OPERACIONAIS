a5155c411929e5977474df653b2fdb768a596260
Livia S <silva.livia1@academico.ifrn.edu.br>
2024-07-16 18:02:15 -0300

    ShellsBar e questão 6

---
+++ IPC/06-QUESTAO/shm.cpp
@@ -0,0 +1,53 @@
+/* produtor/consumidor: Programa exemplo de memória compartilhada */
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    /* Tamanho (em bytes) da memória compartilhada */
+    const int SIZE = 27;
+    /* Nome da área de memória compartilhada */
+    const char *name = "OS";
+    /* fd (File Descriptor) da memória compartilhda */
+    int shm_fd;
+    /* Ponteiro para a área compartilhada */
+    void *ptr;
+
+    /* Cria a área de memória */
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    /* Confifura o tamanho da área */
+    ftruncate(shm_fd, SIZE);
+    /* Mapeia a área para acesso via ponteiro */
+    ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    char *s = static_cast<char *>(ptr);
+    pid_t pid = fork();
+
+    const char *arquivoDestino = argv[1];
+
+    /***************************PRODUTOR*********************************** */
+    if (pid) { /* processo pai: produtor */
+        /* Escreve na área os caracteres de 'a' a 'z' */
+        for (char c = 'a'; c <= 'z'; c++)
+            *s++ = c;
+        *s = (char)NULL;
+        s = static_cast<char *>(ptr);
+        while (*s != '*')
+            sleep(1);
+
+        /* Remove memória compartilhada */
+        shm_unlink(name);
+
+        /***************************consumidor********************************* */
+    } else { /* processo filho: consumidor */
+        const char *programa = "consumidor";
+        const char *size = "27";
+
+        if (execl(programa, programa,name, size , arquivoDestino, NULL)) {
+        perror("execl falhou");
+        }
+    }
+    std::exit(0);
+}
